473a31dbd34e7e657a8519a11bee5452c489b1e2
KevFR <ikevfr@gmail.com>
2025-02-14 13:05:43 +0100

    darkmode working powered "copilot"

--- js/app.js
+++ js/app.js
@@ -1,21 +1,42 @@
-// Theme clair/sombre
+// Variable
+const btnToggle = document.querySelector('.btn-toggle');
+const body = document.body;
 
-const btnToggle = document.querySelector('.btn-toggle')
+// Check si le thème est sauvegardé au chargement de la page
+document.addEventListener('DOMContentLoaded', () => {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+        body.classList.remove('light', 'dark');
+        body.classList.add(savedTheme);
 
+        if (savedTheme === 'dark') {
+            kcLogo.src = './images/misc/kc_darkmode.png';
+            githubIcon.style.color = 'rgb(255, 255, 255)';
+            btnToggle.innerHTML = "Light mode";
+        } else {
+            kcLogo.src = './images/misc/kc_lightmode.png';
+            githubIcon.style.color = 'rgb(0, 0, 0)';
+            btnToggle.innerHTML = "Dark mode (beta)";
+        }
+    }
+});
+
+// Button pour activer le thème
 btnToggle.addEventListener('click', () => {
-    const body = document.body;
-
     if (body.classList.contains('dark')) {
-        document.body.classList.add('light')
-        document.body.classList.remove('dark')
-        btnToggle.innerHTML = "Dark mode (beta)"
+        kcLogo.src = './images/misc/kc_lightmode.png';
+        githubIcon.style.color = 'rgb(0, 0, 0)';
+        body.classList.add('light');
+        body.classList.remove('dark');
+        btnToggle.innerHTML = "Dark mode (beta)";
+        localStorage.setItem('theme', 'light');
     }
     else if (body.classList.contains('light')) {
-        document.body.classList.add('dark')
-        document.body.classList.remove('light')
-        btnToggle.innerHTML = "Light mode"
+        kcLogo.src = './images/misc/kc_darkmode.png';
+        githubIcon.style.color = 'rgb(255, 255, 255)';
+        body.classList.add('dark');
+        body.classList.remove('light');
+        btnToggle.innerHTML = "Light mode";
+        localStorage.setItem('theme', 'dark');
     }
-})
-
-
-
+});
